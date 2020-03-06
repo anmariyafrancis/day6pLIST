ea1fe6b9f636817d56a1e3bcbe0a7e0ead8af2d3
anmariyafrancis <anmariya1915@gmail.com>
2020-03-06 18:56:52 -0500

    PLIST OF EMPLOYEES

--- day6/ViewController.swift
+++ day6/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        readPlistData()
     }
-
-
+    func readPlistData()
+    {
+        if let bundlepath = Bundle.main.path(forResource: "Employee", ofType: "plist")
+        {
+            //print(bundlepath)
+            
+            if let dictionary = NSMutableDictionary(contentsOfFile: bundlepath)
+            {
+                //print(dictionary)
+                if let countries = dictionary["countries"] as? [String]
+                {
+                    for v in countries
+                    {
+                        print(v)
+                    }
+                }
+                if let users = dictionary["users"] as? [[String:String]]
+                {
+                    var flag = false
+                    for userList in users
+                    {
+                        if userList["username"] == "abc" && userList["password"] == "abc@123"
+                        {
+                            flag=true
+                        }
+                    }
+                    if flag==true
+                    {
+                        print("valid user")
+                    }
+                    else
+                    {
+                        print("invalid user")
+                    }
+                }
+            }
+            
+        }
+    }
 }
 
